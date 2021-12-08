f845b522d09702e137010900af0dc020fe32f188
Ken Mueller <kenmueller0@gmail.com>
2021-12-07 23:18:10 -0800

    initial commit

---
+++ routes/__layout.svelte
@@ -0,0 +1,20 @@
+<slot />
+
+<style lang="scss" global>
+	*,
+	::before,
+	::after {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+			Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		border: none;
+		outline: none;
+	}
+
+	html,
+	body {
+		height: 100%;
+	}
+</style>
---
+++ routes/index.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Trail of Trades</title>
+</svelte:head>
+
+<main>
+	<h1>Trail of Trades</h1>
+</main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		height: 100%;
+	}
+
+	h1 {
+		margin: auto;
+	}
+</style>
