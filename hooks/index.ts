6c665079cff4a3260a4c92011d9ef37bd16f2186
Ken Mueller <kenmueller0@gmail.com>
2021-12-10 00:12:36 -0800

    simplfied

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,2 +1,1 @@
-export { default as handle } from './handle'
-export { default as getSession } from './session'
+export {}
--- routes/index.svelte
+++ routes/index.svelte
@@ -1,38 +1,5 @@
-<script lang="ts" context="module">
-	export const load: Load = async ({ fetch, page }) => {
-		try {
-			const response = await fetch(
-				`/companies?filter=${page.query.get(COMPANY_FILTER_KEY) ?? ''}`
-			)
-
-			if (!response.ok)
-				return { status: response.status, error: await response.text() }
-
-			initialCompanies.set(await response.json())
-
-			return {}
-		} catch (error) {
-			return {
-				status: 500,
-				error: error instanceof Error ? error : UNKNOWN_ERROR_MESSAGE
-			}
-		}
-	}
-</script>
-
-<script lang="ts">
-	import type { Load } from '@sveltejs/kit'
-
-	import { COMPANY_FILTER_KEY } from '../lib/company/filter'
-	import initialCompanies from '../lib/company/initial'
-	import UNKNOWN_ERROR_MESSAGE from '../lib/error/unknown'
-	import Navbar from '../components/Navigation/Navbar.svelte'
-	import Companies from '../components/Companies/Scene.svelte'
-</script>
-
 <svelte:head>
 	<title>Trail of Trades</title>
 </svelte:head>
 
-<Navbar />
-<Companies />
+<h1>Trail of Trades</h1>
