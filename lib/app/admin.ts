ba1227a66f35e4c6e42e00a833edb475abb0f634
Ken Mueller <kenmueller0@gmail.com>
2021-12-10 00:30:54 -0800

    get app

--- lib/app/admin.ts
+++ lib/app/admin.ts
@@ -1,19 +1,25 @@
-import type { App, ServiceAccount } from 'firebase-admin/app'
-import { initializeApp, cert } from 'firebase-admin/app'
+import type { FirebaseError, ServiceAccount } from 'firebase-admin/app'
+import {
+	getApp as getDefaultApp,
+	initializeApp,
+	cert
+} from 'firebase-admin/app'
 
-let app: App | null = null
+const getApp = () => {
+	try {
+		return getDefaultApp()
+	} catch (error) {
+		if ((error as FirebaseError)?.code !== 'app/no-app') throw error
 
-const getApp = () =>
-	(app ??= initializeApp({
-		credential: cert(
-			JSON.parse(
-				Buffer.from(
-					import.meta.env.VITE_FIREBASE_ADMIN_KEY,
-					'base64'
-				).toString()
-			) as ServiceAccount
-		),
-		storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET
-	}))
+		const serviceAccount = JSON.parse(
+			Buffer.from(import.meta.env.VITE_FIREBASE_ADMIN_KEY, 'base64').toString()
+		) as ServiceAccount
+
+		return initializeApp({
+			credential: cert(serviceAccount),
+			storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET
+		})
+	}
+}
 
 export default getApp
--- lib/app/index.ts
+++ lib/app/index.ts
@@ -1,17 +1,22 @@
-import type { FirebaseApp } from 'firebase/app'
-import { initializeApp } from 'firebase/app'
+import type { FirebaseError } from 'firebase/app'
+import { getApp as getDefaultApp, initializeApp } from 'firebase/app'
 
-let app: FirebaseApp | null = null
+const getApp = () => {
+	try {
+		return getDefaultApp()
+	} catch (error) {
+		if ((error as FirebaseError)?.code !== 'app/no-app') throw error
 
-const getApp = () =>
-	(app ??= initializeApp({
-		apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-		authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-		projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-		storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-		messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-		appId: import.meta.env.VITE_FIREBASE_APP_ID,
-		measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-	}))
+		return initializeApp({
+			apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+			authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+			projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+			storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+			messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+			appId: import.meta.env.VITE_FIREBASE_APP_ID,
+			measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+		})
+	}
+}
 
 export default getApp
