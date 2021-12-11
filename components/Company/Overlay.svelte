8f1c9b2bb512f8c241389ab70a02a647816ee8d9
Ken Mueller <kenmueller0@gmail.com>
2021-12-11 14:01:52 -0800

    close button

--- components/Company/Overlay.svelte
+++ components/Company/Overlay.svelte
@@ -8,6 +8,7 @@
 
 <main transition:fade={{ duration: 150 }}>
 	<h1>{company.name}</h1>
+	<a href="/">Close</a>
 </main>
 
 <style lang="scss">
@@ -21,7 +22,6 @@
 		bottom: 0;
 		left: 0;
 		padding: 2rem 2.5rem;
-		color: colors.$black;
 		background: transparentize(white, 0.5);
 		backdrop-filter: blur(0.63rem);
 		z-index: z-index.$overlay;
@@ -30,5 +30,13 @@
 	h1 {
 		font-size: 3rem;
 		font-weight: 900;
+		color: colors.$black;
+	}
+
+	a {
+		display: inline-block;
+		margin-top: 1rem;
+		font-weight: 700;
+		color: colors.$black;
 	}
 </style>
