4eea81ff23a22327b43bc1b019acb34344463026
Ken Mueller <kenmueller0@gmail.com>
2021-12-11 16:17:04 -0800

    external css

--- routes/__layout.svelte
+++ routes/__layout.svelte
@@ -2,6 +2,8 @@
 	import Analytics from '../components/Analytics.svelte'
 	import Progress from '../components/Progress.svelte'
 	import Companies from '../components/Companies/Scene.svelte'
+
+	import '../shared/reset.scss'
 </script>
 
 <Analytics />
@@ -9,43 +11,3 @@
 
 <Companies />
 <slot />
-
-<style lang="scss" global>
-	*,
-	::before,
-	::after {
-		box-sizing: border-box;
-		margin: 0;
-		padding: 0;
-		line-height: inherit;
-		font-family: inherit;
-		font-size: inherit;
-		border: none;
-		outline: none;
-	}
-
-	html,
-	body {
-		height: 100%;
-	}
-
-	body {
-		overflow: hidden;
-		line-height: 1;
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-			Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-	}
-
-	button,
-	[role='button'] {
-		cursor: pointer;
-	}
-
-	button:disabled {
-		cursor: default;
-	}
-
-	svg {
-		display: block;
-	}
-</style>
