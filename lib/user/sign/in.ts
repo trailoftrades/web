c8eee2a4f720c7cc4948b7def99fe917c66ddc1b
Ken Mueller <kenmueller0@gmail.com>
2021-12-12 17:28:17 -0800

    do not create user

--- lib/user/sign/in.ts
+++ lib/user/sign/in.ts
@@ -1,35 +1,14 @@
-import {
-	getAuth,
-	GoogleAuthProvider,
-	signInWithPopup,
-	getAdditionalUserInfo
-} from 'firebase/auth'
-import { getFirestore, doc, setDoc } from 'firebase/firestore'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 
 import getApp from '../../app'
-import INITIAL_CASH from '../cash/initial'
 
 const auth = getAuth(getApp())
-const firestore = getFirestore(getApp())
 
 const provider = new GoogleAuthProvider()
 provider.addScope('https://www.googleapis.com/auth/userinfo.email')
 
 const signIn = async () => {
-	const result = await signInWithPopup(auth, provider)
-
-	const { user } = result
-	if (!user.email) throw new Error('Unable to get email')
-
-	const info = getAdditionalUserInfo(result)
-	if (!info) throw new Error('Unable to get additional user info')
-
-	if (info.isNewUser)
-		await setDoc(doc(firestore, `users/${user.uid}`), {
-			name: user.displayName,
-			email: user.email,
-			cash: INITIAL_CASH
-		})
+	await signInWithPopup(auth, provider)
 }
 
 export default signIn
